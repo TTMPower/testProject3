42d1af1fbc09d776abbda2720945253b94c3ffd0
Vladislav Vishyankov <84133235+TTMPower@users.noreply.github.com>
2021-06-21 11:20:11 +0600

    test commit

--- projectHomeWork/ViewController.swift
+++ projectHomeWork/ViewController.swift
@@ -9,8 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let userDefaults = "Defaults"
         // Do any additional setup after loading the view.
     }
 
