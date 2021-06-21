04d527757d5dd9eabb7a960693c0e884b7000ee1
Vladislav Vishyankov <84133235+TTMPower@users.noreply.github.com>
2021-06-21 13:24:49 +0600

    Data storage with userDefaults

---
+++ projectHomeWork/Persistance.swift
@@ -0,0 +1,23 @@
+//
+//  Persistance.swift
+//  projectHomeWork
+//
+//  Created by Владислав Вишняков on 21.06.2021.
+//
+
+import Foundation
+
+class Persistance {
+    static let shared = Persistance()
+    private let firstNameKey = "firstKeyData"
+    private let secondNameKey = "secondKeyData"
+    
+    var firstName: String? {
+        set { UserDefaults.standard.set(newValue, forKey: firstNameKey) }
+        get { return UserDefaults.standard.string(forKey: firstNameKey) }
+    }
+    var secondName: String? {
+        set { UserDefaults.standard.set(newValue, forKey: secondNameKey) }
+        get { return UserDefaults.standard.string(forKey: secondNameKey) }
+    }
+}
--- projectHomeWork/ViewController.swift
+++ projectHomeWork/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 class ViewController: UIViewController {
 
 
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var secondName: UITextField!
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let userDefaults = "Defaults"
-        // Do any additional setup after loading the view.
+        
+        firstName.text = Persistance.shared.firstName
+        secondName.text = Persistance.shared.secondName
+        
+       
+
+    }
+    
+    @IBAction func saveButton(_ sender: Any) {
+        Persistance.shared.firstName = firstName.text
+        Persistance.shared.secondName = secondName.text
+        print(Persistance.shared.firstName)
+        print(Persistance.shared.secondName)
     }
 
 
